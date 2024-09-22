ce340651bc6498bb4076881c37dda43e7fd95b04
chevitaresebruno <chevitarese.bruno@gmail.com>
2024-09-22 19:39:32 -0300

    .

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,6 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MentesInovadoras', // Usually your GitHub org/user name.
   projectName: 'PracaDaCiencia-Docs', // Usually your repo name.
-  deploymentBranch: "deployment",     // Branch for deployment
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
